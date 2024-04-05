40296720094f70f54ba908a1307e2fb44aa1ee9d
Scott Roy <scroy@meta.com>
2024-04-05 10:36:47 -0700

    clean up gguf stuff

--- gguf_util/tests/test_ggml_q40_subclass.py
+++ gguf_util/tests/test_ggml_q40_subclass.py
@@ -8,20 +8,20 @@ import unittest
 
 import torch
 
-from .subclass import from_float, to_float
+from ..ggml_quantization_type import Q4_0
 
 
-class TestGGMLTensorSubclass(unittest.TestCase):
+class TestGGMLQ40TensorSubclass(unittest.TestCase):
     def test_from_float_and_to_float(self) -> None:
         weight = torch.randn([1, 32], dtype=torch.float16)
 
-        packed = from_float(weight)
+        packed = Q4_0.from_float(weight)
 
         self.assertEqual(packed.dtype, torch.uint8)
         # expected size = (1 * sizeof(uint16_t)) + (32 * sizeof(uint8_t)/2) = 18
         self.assertEqual(packed.numel(), 18)
 
-        unpacked = to_float(packed).reshape(weight.shape)
+        unpacked = Q4_0.to_float(packed).reshape(weight.shape)
 
         tolerance = (torch.max(weight) - torch.min(weight)) / 16
 
