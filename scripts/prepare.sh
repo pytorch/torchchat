1311ec093b72aeef1a122a77fbdb67da7d47ebed
Michael Gschwind <mikekg@meta.com>
2024-04-05 19:02:33 -0700

    scipts update

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,1 @@
-python scripts/download.py --repo_id $1 && python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/$1 
+python scripts/download.py --repo-id $1 && python scripts/convert_hf_checkpoint.py --checkpoint-dir checkpoints/$1 
