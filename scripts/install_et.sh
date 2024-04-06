6e01484e57bb94a5335fc7e0951dbdbeba935a02
Scott Roy <scroy@meta.com>
2024-04-06 13:15:36 -0700

    use et-install script

--- scripts/install_et.sh
+++ scripts/install_et.sh
@@ -1,25 +1,33 @@
 echo "Install executorch: cloning"
-
-mkdir build
-cd build
+rm -rf ${LLAMA_FAST_DIR}/build
+mkdir ${LLAMA_FAST_DIR}/build
+cd ${LLAMA_FAST_DIR}/build
+echo "Inside: ${PWD}"
 git clone https://github.com/pytorch/executorch.git
 cd executorch
-
 echo "Install executorch: submodule update"
 git submodule sync
 git submodule update --init
 
+export ET_DIR=${LLAMA_FAST_DIR}/build/executorch
+
 echo "Applying fixes"
-export SCRIPT_DIR=$(dirname $(realpath $0))
-echo "Script dir: ${SCRIPT_DIR}"
-echo "Current dir: ${PWD}"
-cp ${SCRIPT_DIR}/fixes_et/module.h ${PWD}/extension/module/module.h
-cp ${SCRIPT_DIR}/fixes_et/module.cpp ${PWD}/extension/module/module.cpp
+echo "Inside: ${PWD}"
+cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.h ${ET_DIR}/extension/module/module.h
+cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.cpp ${ET_DIR}/extension/module/module.cpp
 
 echo "Install executorch: running pip install"
-sh ./install_requirements.sh --pybind xnnpack
+./install_requirements.sh --pybind xnnpack
 
 echo "Install executorch: building C++ libraries"
+echo "Inside: ${PWD}"
 mkdir cmake-out
 cmake -DCMAKE_BUILD_TYPE=Release -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON -DEXECUTORCH_BUILD_XNNPACK=ON -S . -B cmake-out -G Ninja
 cmake --build cmake-out
+
+echo "Installing runner-et"
+cd ${LLAMA_FAST_DIR}
+echo "Inside: ${PWD}"
+mkdir -p build/cmake-out
+cmake -DET_DIR:STRING=$ET_DIR -DCMAKE_BUILD_TYPE=Release -S runner-et -B build/cmake-out -G Ninja
+cmake --build build/cmake-out
