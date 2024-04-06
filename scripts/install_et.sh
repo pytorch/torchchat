ad6dcc970b1b7559ffc7b84fac1ccdd5ec72ad9a
Scott Roy <scroy@meta.com>
2024-04-06 14:06:33 -0700

    more fixes to ET files

--- scripts/install_et.sh
+++ scripts/install_et.sh
@@ -9,12 +9,13 @@ echo "Install executorch: submodule update"
 git submodule sync
 git submodule update --init
 
-export ET_DIR=${LLAMA_FAST_DIR}/build/executorch
+export ET_DIR=${LLAMA_FAST_DIR}/build
 
 echo "Applying fixes"
 echo "Inside: ${PWD}"
-cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.h ${ET_DIR}/extension/module/module.h
-cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.cpp ${ET_DIR}/extension/module/module.cpp
+cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.h ${ET_DIR}/executorch/extension/module/module.h
+cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.cpp ${ET_DIR}/executorch/extension/module/module.cpp
+cp ${LLAMA_FAST_DIR}/scripts/fixes_et/managed_tensor.h ${ET_DIR}/executorch/extension/runner_util/managed_tensor.h
 
 echo "Install executorch: running pip install"
 ./install_requirements.sh --pybind xnnpack
@@ -29,5 +30,5 @@ echo "Installing runner-et"
 cd ${LLAMA_FAST_DIR}
 echo "Inside: ${PWD}"
 mkdir -p build/cmake-out
-cmake -DET_DIR:STRING=$ET_DIR -DCMAKE_BUILD_TYPE=Release -S runner-et -B build/cmake-out -G Ninja
+cmake -DET_DIR:STRING=$ET_DIR -DCMAKE_BUILD_TYPE=Release -S runner-et -B build/cmake-out
 cmake --build build/cmake-out
