428b2249c0de99b34e06ac1de89d9f966ae89817
Michael Gschwind <mikekg@meta.com>
2024-04-05 19:49:44 -0700

    fix reference to device

--- export.py
+++ export.py
@@ -46,7 +46,7 @@ def main(checkpoint_path, device, quantize = "{ }", args = None):
     print("Loading model ...")
     t0 = time.time()
     model = _load_model(
-        checkpoint_path, device="cpu", precision=precision, use_tp=False)
+        checkpoint_path, device=device, precision=precision, use_tp=False)
 
     device_sync(device=device)  # MKG
     print(f"Time to load model: {time.time() - t0:.02f} seconds")
