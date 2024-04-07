ca9f88a4de5ccf0ef9bdafa1ddc72d093c6ede9f
Scott Roy <scroy@meta.com>
2024-04-07 10:21:16 -0700

    introduce install script

--- scripts/install_et.sh
+++ scripts/install_et.sh
@@ -13,9 +13,8 @@ export ET_DIR=${LLAMA_FAST_DIR}/build
 
 echo "Applying fixes"
 echo "Inside: ${PWD}"
-cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.h ${ET_DIR}/executorch/extension/module/module.h
-cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.cpp ${ET_DIR}/executorch/extension/module/module.cpp
-cp ${LLAMA_FAST_DIR}/scripts/fixes_et/managed_tensor.h ${ET_DIR}/executorch/extension/runner_util/managed_tensor.h
+cp ${LLAMA_FAST_DIR}/scripts/fixes_et/module.cpp ${ET_DIR}/executorch/extension/module/module.cpp # ET uses non-standard C++ that does not compile in GCC
+cp ${LLAMA_FAST_DIR}/scripts/fixes_et/managed_tensor.h ${ET_DIR}/executorch/extension/runner_util/managed_tensor.h # ET is missing headers for vector/memory
 
 echo "Install executorch: running pip install"
 ./install_requirements.sh --pybind xnnpack
@@ -25,3 +24,9 @@ echo "Inside: ${PWD}"
 mkdir cmake-out
 cmake -DCMAKE_BUILD_TYPE=Release -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON -DEXECUTORCH_BUILD_XNNPACK=ON -S . -B cmake-out -G Ninja
 cmake --build cmake-out
+
+cd ${LLAMA_FAST_DIR}
+echo "Inside: ${PWD}"
+mkdir -p build/cmake-out
+cmake -DET_DIR:STRING=${ET_DIR} -DCMAKE_BUILD_TYPE=Release -S runner-et -B build/cmake-out
+cmake --build build/cmake-out
