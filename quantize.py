bd44d460d134507b0b646a1a2206f488a74641ad
Michael Gschwind <61328285+mikekgfb@users.noreply.github.com>
2024-04-10 21:46:04 -0700

    macos12 full build (x86) (#125)
    * macos12 full build (x86)
    
    * add support for setting precision via --dtype

--- quantize.py
+++ quantize.py
@@ -23,6 +23,16 @@ except:
 ##########################################################################
 ###               dtype name to torch.dtype mapping                    ###
 
+precision = torch.float
+
+def set_precision(dtype):
+    global precision
+    precision = dtype
+
+def get_precision():
+    global precision
+    return precision
+
 def name_to_dtype(name):
     if name in name_to_dtype_dict:
         return name_to_dtype_dict[name]
@@ -33,6 +43,11 @@ name_to_dtype_dict = {
     "fp32" : torch.float,
     "fp16" : torch.float16,
     "bf16" : torch.bfloat16,
+    "float" : torch.float,
+    "half" : torch.float16,
+    "float32" : torch.float,
+    "float16" : torch.float16,
+    "bfloat16" : torch.bfloat16,
 }
 
 ##########################################################################
