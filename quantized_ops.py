55da1f13a8bceb72fd51b59e74443d938e4fb4a2
Michael Gschwind <61328285+mikekgfb@users.noreply.github.com>
2024-04-11 02:15:16 -0700

    dtype test (#137)
    * dtype test
    
    * quantized ops fixes
    
    * undo linear8 op
    
    * rename dtype test
    
    * tab->spc
    
    * dtype fix
    
    * disable int4 until https://github.com/pytorch/pytorch/pull/123794 lands
    
    * dtype

--- quantized_ops.py
+++ quantized_ops.py
@@ -7,6 +7,7 @@
 from typing import Optional
 
 import torch
+import torch.nn.functional as F
 from torch.library import impl, impl_abstract
 
 torchat_lib = torch.library.Library(
@@ -27,7 +28,7 @@ def embedding_int8(
     indices = input
     # embedding_byte_weight_checks(weight, weight_scales, weight_zero_points)
     group_size = weight.size(1) // (
-        weight_scales.size(1) if weight_scales.dim() == 2 else 1
+        scales.size(1) if scales.dim() == 2 else 1
     )
     # ET definition
     if False:
@@ -48,7 +49,7 @@ def embedding_int8(
     result_weights = F.embedding(indices, weight)
     result_scales = F.embedding(indices, scales)
 
-    rw_view = result_weights.to(dtype=result_scales.dtype).view(tuple(result_weights.shape[:-1] + (scales.shape[1], -1, )))
+    rw_view = result_weights.to(dtype=result_scales.dtype).view(tuple(result_weights.shape[:-1]) + (scales.shape[1], -1, ))
     rs_view = result_scales.view(tuple(result_scales.shape[:-1]) + (scales.shape[1], 1, ))
     # print(f"rw_view {rw_view.shape}")
     # print(f"rs_view {rs_view.shape}")
@@ -68,7 +69,7 @@ def linear_int8(
         weight: torch.Tensor,
         scales: torch.Tensor,
         bias: Optional[torch.Tensor] = None,
-) -> Tensor:
+) -> torch.Tensor:
     assert bias is None, "bias != None not implemented"
     
     scales = scales.view(scales.shape[0], -1)
@@ -77,7 +78,7 @@ def linear_int8(
     # for now, we special-case channel-wise, because we know how to
     # make that fast with Triton 
     if scales.shape[1] == 1:
-        return F.linear(input, weight.to(dtype=input.dtype)) * self.scales
+        return F.linear(input, weight.to(dtype=input.dtype)) * scales
     else:
         return F.linear(
             input,
@@ -90,7 +91,7 @@ def linear_int8(
 
 torchat_lib.define(
     "linear_int4(Tensor input, Tensor weight, Tensor scales_and_zeros, "
-    "Tensor bias = None, int groupsize, int origin_in_features, "
+    "Tensor bias=None, *, int groupsize, int origin_in_features, "
     "int int_features, int out_features, bool padding = True) -> Tensor",
 )
 
@@ -99,18 +100,17 @@ def linear_int4(
         input: torch.Tensor,
         weight: torch.Tensor,
         scales_and_zeros: torch.Tensor,
-        bias: torch.Tensor = None,
+        bias: torch.Tensor,
         *,
         groupsize: int,
         origin_in_features: int,
         in_features: int,
         out_features: int,
         padding: bool = True,
-) -> Tensor:
+) -> torch.Tensor:
     assert bias is None, "bias != None not implemented"
 
     if padding:
-        import torch.nn.functional as F
         input = F.pad(input, pad=(0, in_features - origin_in_features))
 
     # the weight is in int4pack format
@@ -129,3 +129,44 @@ def linear_int4(
     c = c.reshape(new_shape)
     return c
 
+
+torchat_lib.define(
+    "linear_a8w4dq(Tensor input, Tensor weight, Tensor scales, "
+    "Tensor zeros, int out_features, int group_size, "
+    "dtype precision) -> Tensor",
+)
+
+@impl(torchat_lib, "linear_a8w4dq", "CompositeExplicitAutograd")
+def linear_a8w4dq(
+    input, weight, scales, zeros, out_features, group_size, precision
+):
+    x = per_token_dynamic_quant(input)
+    weight_int8 = weight
+    # TODO: verify and remove following reshape code
+    # origin_x_size = x.size()
+    # x = x.reshape(-1, origin_x_size[-1])
+
+    # TODO: better API
+    # weight_int8 = torch.ops.quantized_decomposed.unpack_int4_to_int8(weight_int4packed)
+    n_bit = 4
+    quant_min = -(2 ** (n_bit - 1))
+    quant_max = 2 ** (n_bit - 1) - 1
+    w_dq = torch.ops.quantized_decomposed.dequantize_per_channel_group(
+        weight_int8,
+        scales,
+        zeros,
+        quant_min,
+        quant_max,
+        torch.int8,
+        group_size,
+        precision,
+    )
+
+    # x = x.to(torch.float16)
+    # w_dq = w_dq.to(torch.float16)
+    c = torch.nn.functional.linear(x, w_dq)
+
+    # new_shape = origin_x_size[:-1] + (out_features,)
+    # c = c.reshape(new_shape)
+
+    return c
