741d7ba084cac86a41139f8a06136b89c7a7275a
Guang Yang <42389959+guangy10@users.noreply.github.com>
2024-04-12 12:12:38 -0700

    Fix loading checkpoints with fused wqkv weights (#158)

--- scripts/test_flow.sh
+++ scripts/test_flow.sh
@@ -2,4 +2,4 @@ export MODEL_REPO=meta-llama/Llama-2-7b-chat-hf
 rm -r checkpoints/$MODEL_REPO
 python scripts/download.py --repo-id $MODEL_REPO
 python scripts/convert_hf_checkpoint.py --checkpoint-dir checkpoints/$MODEL_REPO
-python generate.py --compile --checkpoint-path checkpoints/$MODEL_REPO/model.pth --max_new_tokens 100
+python generate.py --compile --checkpoint-path checkpoints/$MODEL_REPO/model.pth --max-new-tokens 100
