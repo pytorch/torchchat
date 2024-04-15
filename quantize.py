7ebf5a2a301a1055e5a7980899c5f68f233fae51
Michael Gschwind <mikekg@meta.com>
2024-04-14 18:37:40 -0700

    typo

--- quantize.py
+++ quantize.py
@@ -506,7 +506,7 @@ def replace_embedding_weight_only_grouped_int8_per_channel(
                     vocab_size=child.weight.shape[0],
                     embedding_dim=child.weight.shape[1],
                     group_size=group_size,
-                    packed
+                    packed=packed,
                 ),
             )
         else:
