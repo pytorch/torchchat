1f7f01ec0b3c9ed53277f06908720c90b3f39f51
Kimish Patel <kimishpatel@fb.com>
2024-04-22 14:03:23 -0700

    Enable android runner with custom sdpa op (#363)
    Summary:
    This diff
    - refactors install_et.sh into a bunch of utils
    - Uses those utils in build_android.sh to minimize duplication.
    - Makes sure taht we are building with custom sdpa op
    
    Test Plan:
    
    Model export
    python export.py --quant '{"linear:a8w4dq" : {"groupsize": 256}}'
    --checkpoint-path /home/kimishpatel/models/llama2/stories/stories110M.pt
    --params-path /home/kimishpatel/models/llama2/stories/params.json
    --output-pte-path /tmp/stories110m_a8w4dq.pte
    python utils/tokenizer.py --tokenizer-model=/tmp/tokenizer.model
    
    linux:
    ./scripts/install_et.sh
    rm -rf build/cmake-out/
    cmake -S ./runner-et -B build/cmake-out -G Ninja
    cmake --build ./build/cmake-out
    
    ./build/cmake-out/runner_et /tmp/stories110m_a8w4dq.pte -z /tmp/tokenizer.bin -t 0 -n 120
    
    android:
    ./runner-et/build_android.sh
    adb push ./build/cmake-out-android/runner_et /data/local/tmp/
    adb push /tmp/stories110m_a8w4dq.pte /data/local/tmp/
    adb push /tmp/tokenizer.bin /data/local/tmp/
    adb shell "cd /data/local/tmp && ./runner_et ./stories110m_a8w4dq.pte -z
    ./tokenizer.bin -t 0 -n 120"
    
    Will add build commands to ci in the next PR
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- runner-et/build_android.sh
+++ runner-et/build_android.sh
@@ -7,6 +7,8 @@
 
 set -ex
 
+source "$(dirname "${BASH_SOURCE[0]}")/../scripts/install_utils.sh"
+
 if ["${ANDROID_NDK}" == ""]; then
   echo "Please set ANDROID_NDK enviornment variable."
   echo "For example it can be /Users/guest/Desktop/android-ndk-r26."
@@ -17,45 +19,24 @@ else
 fi
 
 export CMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
-export DANDROID_ABI=arm64-v8a
-export DANDROID_PLATFORM=android-23 
+export ANDROID_ABI=arm64-v8a
+export ANDROID_PLATFORM=android-23
 export ET_BUILD_DIR="et-build-android"
+export CMAKE_OUT_DIR="cmake-out-android"
+export EXECUTORCH_BUILD_CUSTOM_OPS_AOT="OFF"
+export EXECUTORCH_BUILD_CUSTOM="ON"
 export CMAKE_OUT_DIR="cmake-out-android"
 # export DCMAKE_INSTALL_PREFIX=cmake-out-android
 #
 
-install_executorch() {
-  echo "Cloning executorch to ${TORCHCHAT_ROOT}/${ET_BUILD_DIR}/src"
-  ET_BUILD_DIR="${TORCHCHAT_ROOT}/${ET_BUILD_DIR}"
-  rm -rf ${ET_BUILD_DIR}
-  mkdir -p ${ET_BUILD_DIR}/src
-  pushd ${ET_BUILD_DIR}/src
-  git clone https://github.com/pytorch/executorch.git
-  cd executorch
-  git checkout viable/strict
-  echo "Install executorch: submodule update"
-  git submodule sync
-  git submodule update --init
-
-  echo "Applying fixes"
-  cp ${TORCHCHAT_ROOT}/scripts/fixes_et/module.cpp ${ET_BUILD_DIR}/src/executorch/extension/module/module.cpp # ET uses non-standard C++ that does not compile in GCC
-  cp ${TORCHCHAT_ROOT}/scripts/fixes_et/managed_tensor.h ${ET_BUILD_DIR}/src/executorch/extension/runner_util/managed_tensor.h # ET is missing headers for vector/memory.  This causes downstream issues when building runner-et.
-
-  CMAKE_OUT_DIR="cmake-out-android"
-  echo "Building and installing C++ libraries"
-  echo "Inside: ${PWD}"
-  mkdir ${CMAKE_OUT_DIR}
-  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-23 -DCMAKE_INSTALL_PREFIX=cmake-out-android -DEXECUTORCH_ENABLE_LOGGING=ON -DEXECUTORCH_LOG_LEVEL=Info -DEXECUTORCH_BUILD_OPTIMIZED=ON -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON -DEXECUTORCH_BUILD_XNNPACK=ON -S . -B ${CMAKE_OUT_DIR} -G Ninja
-  cmake --build ${CMAKE_OUT_DIR}
-  cmake --install ${CMAKE_OUT_DIR} --prefix ${ET_BUILD_DIR}/install
-  popd
-}
-
 build_runner_et() {
   rm -rf build/cmake-out-android
+  echo "ET BUILD DIR IS ${ET_BUILD_DIR}"
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-23 -S ./runner-et -B build/cmake-out-android -G Ninja
   cmake --build build/cmake-out-android/ -j16 --config Release
 }
 
-# install_executorch
+find_cmake_prefix_path
+clone_executorch
+install_executorch
 build_runner_et
