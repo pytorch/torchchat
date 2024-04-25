9b998796e9d407690376a982d3ace1372bdebd71
Hansong Zhang <hsz@fb.com>
2024-04-25 12:00:32 -0700

    Add checksum in Android prebuilt

--- scripts/android_example.sh
+++ scripts/android_example.sh
@@ -29,6 +29,10 @@ LLAMA_JNI_ARM64_URL="https://ossci-android.s3.us-west-1.amazonaws.com/executorch
 LLAMA_JNI_X86_64_URL="https://ossci-android.s3.us-west-1.amazonaws.com/executorch/release/0.2/x86_64/libexecutorch_llama_jni.so"
 LLAMA_JAR_URL="https://ossci-android.s3.us-west-1.amazonaws.com/executorch/release/0.2/executorch.jar"
 
+LLAMA_JNI_ARM64_SHASUM="38788e2a3318075ccdd6e337e4c56f82bbbce06f"
+LLAMA_JNI_X86_64_SHASUM="cdc98d468b8e48c8784408fcada8177e6bc5f981"
+LLAMA_JAR_SHASUM="fb9ee00d028ef23a48cb8958638a5010ba849ccf"
+
 mkdir -p ${TORCHCHAT_ROOT}/build/android
 
 setup_java() {
@@ -78,6 +82,7 @@ setup_android_ndk() {
 download_jar_library() {
   mkdir -p ${TORCHCHAT_ROOT}/build/android
   curl "${LLAMA_JAR_URL}" -o ${TORCHCHAT_ROOT}/build/android/executorch.jar
+  echo "${LLAMA_JAR_SHASUM}  ${TORCHCHAT_ROOT}/build/android/executorch.jar" | shasum --check --status
 }
 
 download_jni_library() {
@@ -85,9 +90,11 @@ download_jni_library() {
   mkdir -p ${TORCHCHAT_ROOT}/build/android/x86_64
   if [ ! -f ${TORCHCHAT_ROOT}/build/android/arm64-v8a/libexecutorch_llama_jni.so ]; then
     curl "${LLAMA_JNI_ARM64_URL}" -o ${TORCHCHAT_ROOT}/build/android/arm64-v8a/libexecutorch_llama_jni.so
+    echo "${LLAMA_JNI_ARM64_SHASUM}  ${TORCHCHAT_ROOT}/build/android/arm64-v8a/libexecutorch_llama_jni.so" | shasum --check --status
   fi
   if [ ! -f ${TORCHCHAT_ROOT}/build/android/x86_64/libexecutorch_llama_jni.so ]; then
     curl "${LLAMA_JNI_X86_64_URL}" -o ${TORCHCHAT_ROOT}/build/android/x86_64/libexecutorch_llama_jni.so
+    echo "${LLAMA_JNI_X86_64_SHASUM}  ${TORCHCHAT_ROOT}/build/android/x86_64/libexecutorch_llama_jni.so" | shasum --check --status
   fi
 }
 
