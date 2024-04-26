c29e75ec4f9906e2f07be0566a3135682368cad6
kirklandsign <107070759+kirklandsign@users.noreply.github.com>
2024-04-26 11:43:37 -0700

    Minimal android app build (#491)
    * Minimal android app build
    
    * Improve script
    
    * Detect physical device as well

---
+++ android/Torchchat/app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "org.pytorch.torchchat"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "org.pytorch.torchchat"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.facebook.fbjni:fbjni:0.5.1")
+    implementation(files("libs/executorch.aar"))
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
