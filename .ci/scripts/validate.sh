3ee001e9c82ca78264da14e187ee10294ed360f6
metascroy <161522778+metascroy@users.noreply.github.com>
2024-04-27 08:55:42 -0700

    break aoti CI jobs separately (#500)
    * init
    
    * fixes
    
    * more fixes
    
    * fixes
    
    * fix
    
    * fix
    
    * bug fix
    
    * add objcopy update
    
    * suppress int8
    
    * undefined variable
    
    ---------
    
    Co-authored-by: Michael Gschwind <mikekg@meta.com>

--- .ci/scripts/validate.sh
+++ .ci/scripts/validate.sh
@@ -25,6 +25,7 @@ function generate_compiled_model_output() {
     local MODEL_DIR="${CHECKPOINT_PATH%/*}"
     local MODEL_NAME=$(basename "$CHECKPOINT_PATH" | sed 's/\.[^.]*$//')
 
+
     if [[ $CHECKPOINT_PATH != *"stories"* && $TARGET_DEVICE == "cuda" ]]; then
         DTYPES="bfloat16"
         EXCLUDE_INT8_QUANT=true
@@ -74,7 +75,7 @@ function generate_compiled_model_output() {
         python3 -W ignore generate.py --dtype ${DTYPE} --compile --quant '{"embedding" : {"bitwidth": 4, "groupsize": 8, "packed": "True"}}' --checkpoint-path "$CHECKPOINT_PATH" --temperature 0 --device "$TARGET_DEVICE" > "$MODEL_DIR/output_compiled" || exit 1
         cat "$MODEL_DIR/output_compiled"
 
-        if [ "$EXCLUDE_INT8_QUANT" = false ]; then
+        if [ "${EXCLUDE_INT8_QUANT:-false}" == false ]; then
             echo "******************************************"
             echo "******* INT8 channel-wise quantized ******"
             echo "******************************************"
@@ -109,16 +110,23 @@ function generate_compiled_model_output() {
 function generate_aoti_model_output() {
     local CHECKPOINT_PATH="$1"
     local TARGET_DEVICE="${2:-cpu}"
+    local DTYPES="${3:-default}"
     local MODEL_DIR="${CHECKPOINT_PATH%/*}"
     local MODEL_NAME=$(basename "$CHECKPOINT_PATH" | sed 's/\.[^.]*$//')
 
-    if [[ $CHECKPOINT_PATH != *"stories"* && $TARGET_DEVICE == "cuda" ]]; then
-        DTYPES="bfloat16"
-        EXCLUDE_INT8_QUANT=true
-    else
-        DTYPES="float32 bfloat16 float16"
-        EXCLUDE_INT8_QUANT=false
+    echo "Local DTYPES=$DTYPES"
+
+    if [[ $DTYPES == "default" ]]; then
+        if [[ $CHECKPOINT_PATH != *"stories"* && $TARGET_DEVICE == "cuda" ]]; then
+            DTYPES="bfloat16"
+            EXCLUDE_INT8_QUANT=true
+        else
+            DTYPES="float32 bfloat16 float16"
+            EXCLUDE_INT8_QUANT=false
+        fi
     fi
+
+    echo "Local after default DTYPES=$DTYPES"
 
     for DTYPE in $DTYPES; do
         echo ""############### Run inference with AOT Inductor  for dtype $DTYPE "###############"
@@ -158,7 +166,7 @@ function generate_aoti_model_output() {
         python3 -W ignore generate.py --dtype ${DTYPE} --checkpoint-path "$CHECKPOINT_PATH" --temperature 0 --dso-path ${MODEL_DIR}/${MODEL_NAME}.so --device "$TARGET_DEVICE" > "$MODEL_DIR/output_aoti" || exit 1
         cat "$MODEL_DIR/output_aoti"
 
-        if [ "$EXCLUDE_INT8_QUANT" = false ]; then
+        if [ "${EXCLUDE_INT8_QUANT:-false}" == false ]; then
             echo "******************************************"
             echo "******* INT8 channel-wise quantized ******"
             echo "******************************************"
@@ -295,11 +303,12 @@ function run_compile() {
 }
 
 function run_aoti() {
-    generate_aoti_model_output "$CHECKPOINT_PATH" "$TARGET_DEVICE" || exit 1
+    echo "Passing DTYPES=$DTYPES"
+    generate_aoti_model_output "$CHECKPOINT_PATH" "$TARGET_DEVICE" "$DTYPES" || exit 1
 }
 
 function run_executorch() {
-    if [ "$TARGET_DEVICE" = "cpu" ]; then
+    if [ "$TARGET_DEVICE" == "cpu" ]; then
         generate_executorch_model_output "$CHECKPOINT_PATH" "$TARGET_DEVICE" || exit 1
     else
         echo "Skipped: Executorch doesn't run on ${TARGET_DEVICE}"
@@ -318,24 +327,44 @@ CHECKPOINT_PATH="$1"
 TARGET_DEVICE="${2:-cpu}"
 PROMPT="Hello, my name is"
 
-
 if [ "$#" -gt 2 ]; then
     # Additional arguments provided
     for arg in "${@:3}"; do
         case "$arg" in
             "compile")
+                echo "arg:$arg"
                 run_compile || exit 1
                 ;;
             "aoti")
+                echo "arg:$arg"
+                DTYPES="default"
+                run_aoti || exit 1
+                ;;
+            "aoti-bfloat16")
+                echo "arg:$arg"
+                DTYPES="bfloat16"
+                run_aoti || exit 1
+                ;;
+            "aoti-float16")
+                echo "arg:$arg"
+                DTYPES="float16"
+                run_aoti || exit 1
+                ;;
+            "aoti-float32")
+                echo "arg:$arg"
+                DTYPES="float32"
                 run_aoti || exit 1
                 ;;
             "executorch")
+                echo "arg:$arg"
                 run_executorch || exit 1
                 ;;
             "eval")
+                echo "arg:$arg"
                 run_eval || exit 1
                 ;;
             "eval_sanity_check")
+                echo "arg:$arg"
                 run_eval_sanity_check || exit 1
                 ;;
             *)
