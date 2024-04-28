f799445f9d9dfc9791fb49ee885f6edfb1288c14
metascroy <161522778+metascroy@users.noreply.github.com>
2024-04-28 15:26:40 -0700

    clean up runner code a little (#532)
    * clean up runner code a little
    
    * update
    
    * update
    
    * pull out generate loop in chat
    
    * updates
    
    * edit docs
    
    * typo

--- scripts/build_native.sh
+++ scripts/build_native.sh
@@ -76,4 +76,4 @@ fi
 cmake -S . -B ./cmake-out -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` -G Ninja
 cmake --build ./cmake-out --target "${TARGET}"_run
 
-printf "Build finished. Please run: \n./cmake-out/${TARGET}_run model.<pte|so> -z tokenizer.model -i <prompt>"
+printf "Build finished. Please run: \n./cmake-out/${TARGET}_run model.<pte|so> -z tokenizer.model -l <llama version (2 or 3)> -i <prompt>"
