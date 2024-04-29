7fc4d9d64ab5c11cbc2e12f43e1403334005ddb2
Michael Gschwind <mikekg@meta.com>
2024-04-29 11:46:01 -0700

    typo

--- export_et_util.py
+++ export_et_util.py
@@ -9,7 +9,7 @@ class CustomKVCache(nn.Module):
     def __init__(self, max_batch_size, max_seq_length, n_heads, head_dim, dtype):
         super().__init__()
 
-        dtype - torch.float
+        dtype = torch.float
         
         # This is flipped around from what is in build.model's KVCache
         cache_shape = (max_batch_size, max_seq_length, n_heads, head_dim)
