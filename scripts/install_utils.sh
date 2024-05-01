1ea9d4048f6c9a58d6b09260d7f2f6beadd92b40
Scott Roy <scroy@meta.com>
2024-04-30 20:01:14 -0700

    change et branch

--- scripts/install_utils.sh
+++ scripts/install_utils.sh
@@ -27,7 +27,7 @@ clone_executorch() {
   pushd ${TORCHCHAT_ROOT}/${ET_BUILD_DIR}/src
   git clone https://github.com/pytorch/executorch.git
   cd executorch
-  git checkout viable/strict
+  git checkout main
   echo "Install executorch: submodule update"
   git submodule sync
   git submodule update --init
