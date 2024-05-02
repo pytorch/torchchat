b964ef8e1523bcc27dd21911a787f502cc22a46e
Michael Gschwind <61328285+mikekgfb@users.noreply.github.com>
2024-05-01 18:09:57 -0700

    remove debug print statements and run linter (#623)

--- qops.py
+++ qops.py
@@ -46,19 +46,37 @@ def linear_int8_aoti(input, weight, scales):
 
 
 def _qdq_dynamic_quantized_linear(
-    x_fp32, x_quant_min, x_quant_max, x_eps,
-    weight_i8, weight_scale, weight_zero_point, weight_quant_min, weight_quant_max,
+    x_fp32,
+    x_quant_min,
+    x_quant_max,
+    x_eps,
+    weight_i8,
+    weight_scale,
+    weight_zero_point,
+    weight_quant_min,
+    weight_quant_max,
     bias_fp32,
 ):
-    x_scale, x_zero_point = torch.ops.quantized_decomposed.choose_qparams(x_fp32, x_quant_min, x_quant_max, x_eps, torch.int8)
+    x_scale, x_zero_point = torch.ops.quantized_decomposed.choose_qparams(
+        x_fp32, x_quant_min, x_quant_max, x_eps, torch.int8
+    )
     x_i8 = torch.ops.quantized_decomposed.quantize_per_tensor(
-        x_fp32, x_scale, x_zero_point, x_quant_min, x_quant_max, torch.int8)
+        x_fp32, x_scale, x_zero_point, x_quant_min, x_quant_max, torch.int8
+    )
     x_fp32 = torch.ops.quantized_decomposed.dequantize_per_tensor(
-        x_i8, x_scale, x_zero_point, x_quant_min, x_quant_max, torch.int8)
+        x_i8, x_scale, x_zero_point, x_quant_min, x_quant_max, torch.int8
+    )
     weight_fp32 = torch.ops.quantized_decomposed.dequantize_per_tensor(
-        weight_i8, weight_scale, weight_zero_point, weight_quant_min, weight_quant_max, torch.int8)
+        weight_i8,
+        weight_scale,
+        weight_zero_point,
+        weight_quant_min,
+        weight_quant_max,
+        torch.int8,
+    )
     out_fp32 = torch.ops.aten.linear.default(x_fp32, weight_fp32, bias_fp32)
     return out_fp32
+
 
 def linear_int8_et(input, weight, scales):
     n_groups = scales.numel() // scales.shape[0]
