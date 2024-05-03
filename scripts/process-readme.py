590c789242641cf534907c89cec7bb5670e14bfc
Michael Gschwind <mikekg@meta.com>
2024-05-02 23:36:43 -0700

    fix errors in updown language

--- scripts/process-readme.py
+++ scripts/process-readme.py
@@ -1,30 +1,29 @@
-
 import sys
 
+
 def print_between_triple_backticks(filename, predicate):
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         lines = file.readlines()
     print_flag = False
     for line in lines:
-        command=f"[shell {predicate}]:"
-        prefix=f"[prefix {predicate}]:"
-        end=f"[end {predicate}]:"
+        command = f"[shell {predicate}]:"
+        prefix = f"[prefix {predicate}]:"
+        end = f"[end {predicate}]:"
         if line.startswith(prefix):
-            print(line[len(prefix):-1], end='')
+            print(line[len(prefix) : -1], end="")
         elif line.startswith(command):
-            print(line[len(command):])
+            print(line[len(command) :])
         elif line.startswith(end):
-            return;
-        elif line.startswith('```'):
+            return
+        elif line.startswith("```"):
             print_flag = not print_flag
         elif print_flag:
-            print(line, end='')
+            print(line, end="")
 
 
-if len(sys.argv )>1:
-    predicate=sys.argv[1]
+if len(sys.argv) > 1:
+    predicate = sys.argv[1]
 else:
-    predicate="default"
-    
-print_between_triple_backticks('README.md', predicate)
+    predicate = "default"
 
+print_between_triple_backticks("README.md", predicate)
