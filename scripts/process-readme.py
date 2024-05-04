824a7abb0df0d437f4adc583780378318f7ec47a
Michael Gschwind <61328285+mikekgfb@users.noreply.github.com>
2024-05-03 21:41:47 -0700

    executable README (#632)
    * executable README
    
    * fix title of CI workflow
    
    * markup commands in markdown
    
    * extend the markup-markdown language
    
    * improvements
    
    * update readme from doc branch
    
    * tab/spc
    
    * fix errors in updown language
    
    * fix errors in updown language, and [skip]: begin/end
    
    * fix errors in updown language, and [skip]: begin/end
    
    * a storied run
    
    * stories run on readme instructions does not need HF token
    
    * increase timeout
    
    * check for hang un hf_login
    
    * skip hf login in case it hangs
    
    * skip checkout
    
    * don't skip login

--- scripts/process-readme.py
+++ scripts/process-readme.py
@@ -1,12 +1,39 @@
-def print_between_triple_backticks(filename):
+import sys
+
+
+def print_between_triple_backticks(filename, predicate):
     with open(filename, "r") as file:
         lines = file.readlines()
     print_flag = False
-    for line in lines:
-        if line.startswith("```"):
+    for i, line in enumerate(lines):
+        command = f"[shell {predicate}]:"
+        prefix = f"[prefix {predicate}]:"
+        end = f"[end {predicate}]:"
+        skip = f"[skip {predicate}]:"
+        if line.startswith(prefix):
+            print(line[len(prefix) : -1], end="")
+        elif line.startswith(command):
+            print(line[len(command) :])
+        elif line.startswith(end):
+            return
+        elif line.startswith(skip):
+            keyword = line[len(skip):-1].strip()
+            if keyword == "begin":
+                print("if false; then")
+            elif keyword == "end":
+                print("fi")
+            else:
+                print(f"echo 'error in line {i} of README.md'")
+                return
+        elif line.startswith("```"):
             print_flag = not print_flag
         elif print_flag:
             print(line, end="")
 
 
-print_between_triple_backticks("README.md")
+if len(sys.argv) > 1:
+    predicate = sys.argv[1]
+else:
+    predicate = "default"
+
+print_between_triple_backticks("README.md", predicate)
