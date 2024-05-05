2b42eed1e28721de57c1043e32ae645c8ba78412
Michael Gschwind <61328285+mikekgfb@users.noreply.github.com>
2024-05-05 09:06:53 -0700

    fix cmake version, padding for a8w4dq, lint... (#680)
    * fix cmake version, p[adding for a8w4dq, lint...
    
    * fix cmake version, p[adding for a8w4dq, lint...
    
    * updates
    
    * fix

--- cli.py
+++ cli.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import json
+import logging
 import os
 from pathlib import Path
 
@@ -12,6 +13,13 @@ import torch
 
 from build.utils import allowable_dtype_names, allowable_params_table, get_device_str
 from download import download_and_convert, is_model_downloaded
+
+FORMAT = (
+    "%(levelname)s: %(asctime)-15s: %(filename)s: %(funcName)s: %(module)s: %(message)s"
+)
+logging.basicConfig(filename="/tmp/torchchat.log", level=logging.INFO, format=FORMAT)
+logger = logging.getLogger(__name__)
+
 
 default_device = "fast"
 default_model_dir = Path(
@@ -316,7 +324,7 @@ def arg_init(args):
     if args.output_pte_path:
         if args.device not in ["cpu", "fast"]:
             raise RuntimeError("Device not supported by ExecuTorch")
-        args.device="cpu"
+        args.device = "cpu"
     else:
         args.device = get_device_str(
             args.quantize.get("executor", {}).get("accelerator", args.device)
