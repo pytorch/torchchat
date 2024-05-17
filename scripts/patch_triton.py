ca65f05559bc6030169a12a11256f20effb21d75
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2024-05-17 16:54:53 -0700

    Patch triton (#820)
    By patching runtime/jit.py with the contents of https://github.com/triton-lang/triton/pull/3564
    
    Should fix intermittent
    ```
    SyntaxError: unterminated string literal (detected at line 1)
    ```
    for good
    
    TODO: Delete `scripts/patch_trition.py` as well as changes to `install_requirements.sh` for good

---
+++ scripts/patch_triton.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Delete me after triton is updated past https://github.com/triton-lang/triton/pull/3564
+
+
+from pathlib import Path
+import triton
+
+
+def patch_def_search_in_jit_py(jit_py: Path) -> None:
+  with jit_py.open() as f:
+    lines = f.readlines()
+  old_match = 'self.src = self.src[self.src.find("def"):]'
+  new_match = 'self.src = self.src[re.search(r"^def\s+\w+\s*\(", self.src, re.MULTILINE).start():]'
+  lines.insert(4, "import re\n")
+  for idx, line in enumerate(lines):
+    if old_match in line:
+      lines[idx] = line.replace(old_match, new_match) 
+  jit_py.write_text("".join(lines))
+
+jit_py = Path(triton.__file__).parent / "runtime" / "jit.py"
+patch_def_search_in_jit_py(jit_py)
+
