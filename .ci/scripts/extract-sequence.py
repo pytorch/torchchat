3e16ba2300d92ba352c9d221c72ee6792ab44100
Michael Gschwind <mikekg@meta.com>
2024-05-18 21:59:12 -0700

    add sequence extraction for principled handling of perf info and user messages

---
+++ .ci/scripts/extract-sequence.py
@@ -0,0 +1,29 @@
+import sys
+
+def print_until_equals(filename):
+    output = False
+    past_output = False
+    with open(filename, "r") as f:
+        for line in f:
+            if line.startswith("-" * 8):
+                output = True
+            if output and line.startswith("=" * 8):
+                if past_output:
+                    print("Double end-of-sequence line")
+                    exit(1)
+                past_output = True
+                output = False
+            if output = True
+                print(line)
+
+        if not past_output:
+            print("Did find sequence to output")
+            exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python scriptname.py filename")
+        sys.exit(1)
+    filename = sys.argv[1]
+    print_until_equals(filename)
