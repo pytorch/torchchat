914914c2f9ced992edf59f8446142b9bce863bef
Michael Gschwind <61328285+mikekgfb@users.noreply.github.com>
2024-05-20 10:22:51 -0700

    Minor editorial fixes (#833)
    * editorial fixes
    
    * update usage message

--- .ci/scripts/extract-sequence.py
+++ .ci/scripts/extract-sequence.py
@@ -23,7 +23,7 @@ def print_until_equals(filename):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python scriptname.py filename")
+        print(f"Usage:\n {sys.executable} {sys.argv[0]} filename")
         sys.exit(1)
     filename = sys.argv[1]
     print_until_equals(filename)
