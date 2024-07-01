c2dbd20d5d1e8a44cf7a0c3feffbfb21c1cfc646
fduwjj <fduwjj@gmail.com>
2024-07-01 10:06:11 -0700

    Remove unnecessary changes

--- config/model_config.py
+++ config/model_config.py
@@ -3,7 +3,6 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-
 import json
 from dataclasses import dataclass, field
 from enum import Enum
