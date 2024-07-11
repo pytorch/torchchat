8daa4ced87e5447e96a84cacf270fd292261c556
Hugo <6937752+fduwjj@users.noreply.github.com>
2024-07-11 12:44:31 -0700

    [Dist][Inference] Enable distributed checkpoint loading for large model (#883)
    * [Dist][Inference] Explore checkpoint loading

--- cli.py
+++ cli.py
@@ -110,6 +110,12 @@ def add_arguments_for_verb(parser, verb: str) -> None:
         help="Use the specified model checkpoint path",
     )
     parser.add_argument(
+        "--dcp-dir",
+        type=Path,
+        default=None,
+        help="Use the specified model checkpoint directory",
+    )
+    parser.add_argument(
         "--params-path",
         type=Path,
         default=None,
