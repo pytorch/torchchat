6372e366c3d762649895f15dad96febe0eecb9b2
Jesse White <5281939+byjlw@users.noreply.github.com>
2024-07-16 09:53:58 -0700

    need license header on every file (#904)
    Add license header to files (#905)
    
    * need license header on every file
    
    * apparently the validator only reports the first violation it finds not all of them
    
    * typeo
    
    don't put the license on these files (#909)
    
    add license to source files (#910)
    
    this should be the final one (#911)

--- build/model_et.py
+++ build/model_et.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import torch
 import torch.nn as nn
 from executorch.extension.pybindings import portable_lib as exec_lib
