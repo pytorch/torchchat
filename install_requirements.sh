4da2ef9a04e1e90fcec890ad2f1d9c529d673fa5
Mengwei Liu <larryliu@meta.com>
2024-07-15 21:57:20 -0700

    Incorporate torchao changes
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- install_requirements.sh
+++ install_requirements.sh
@@ -73,7 +73,7 @@ $PIP_EXECUTABLE install --extra-index-url "${TORCH_NIGHTLY_URL}" \
 
 # For torchao need to install from github since nightly build doesn't have macos build.
 # TODO: Remove this and install nightly build, once it supports macos
-$PIP_EXECUTABLE install git+https://github.com/pytorch/ao.git@ab4ec436f07d391c52d49817622c6622e832aad2
+$PIP_EXECUTABLE install git+https://github.com/pytorch/ao.git@4d9d3bc833f71bdd403d2d9036b0c41e996fdc8a
 if [[ -x "$(command -v nvidia-smi)" ]]; then
   $PYTHON_EXECUTABLE scripts/patch_triton.py
 fi
