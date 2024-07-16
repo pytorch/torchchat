cda86edcce58b9cd849b180a70642994c94935be
Jesse <jessewhite007@yahoo.com>
2024-07-16 15:21:25 -0700

    this should be the final one

--- runner/et.cmake
+++ runner/et.cmake
@@ -1,3 +1,10 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 cmake_minimum_required(VERSION 3.24)
 set(CMAKE_CXX_STANDARD 17)
 
