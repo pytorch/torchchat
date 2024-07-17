9c0f7c1ca4c72f6ca7fa2c0b6deae197a01a45a7
Jesse White <5281939+byjlw@users.noreply.github.com>
2024-07-16 20:27:13 -0700

    this should be the final one (#911)

--- scripts/test_flow.sh
+++ scripts/test_flow.sh
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 export MODEL_REPO=meta-llama/Llama-2-7b-chat-hf
 rm -r checkpoints/$MODEL_REPO
 python3 scripts/download.py --repo-id $MODEL_REPO
