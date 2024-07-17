b81f83e7ff13bd50bbbad0df340047c39987c62a
Nikita Shulga <nshulga@meta.com>
2024-07-17 06:46:55 -0700

    Remove unused/temporary code

--- unsupported/llama2.c/runner-aoti/run.cpp
+++
@@ -1,6 +0,0 @@
-/* Inference for Llama-2 Transformer model in pure C */
-/* this uses the same logic regardless of AOTI OR ET */
-/* but requires different data types - ATen vs ETen  */
-
-#define __AOTI__MODEL
-#include "../runner/run.cpp"
