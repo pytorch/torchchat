87949415b68b6b258d377031ae7c0f694378a298
Mengwei Liu <larryliu@meta.com>
2024-07-17 00:24:04 -0700

    Try something

--- quantization/quantize.py
+++ quantization/quantize.py
@@ -63,6 +63,7 @@ def quantize_model(model: nn.Module, device, quantize_options, tokenizer=None):
             # Only use quant API for dtype bf16 and CUDA
             if quantizer == "linear:int4" and precision == torch.bfloat16 and device == "cuda":
                 quantize_(model, int4_weight_only(group_size=q_kwargs["groupsize"]))
+                model.to(device=device)
                 continue
             
             try:
