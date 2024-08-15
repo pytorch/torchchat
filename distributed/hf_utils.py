06b3cd435539c7df6cf92b95faf55943a0a6ee89
lessw2020 <lessw@etrillium.com>
2024-08-15 15:56:33 -0700

    ruff formatting

--- distributed/hf_utils.py
+++ distributed/hf_utils.py
@@ -1,7 +1,6 @@
-
 from typing import Dict, Callable, Optional, Tuple
 import torch
-from transformers import AutoTokenizer # AutoConfig
+from transformers import AutoTokenizer  # AutoConfig
 from safetensors import safe_open
 from transformers.utils import cached_file
 import logging
@@ -11,7 +10,9 @@ import torch.distributed as dist
 import time
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 # default hf cache lives in ~/.cache/huggingface/hub
@@ -19,59 +20,69 @@ logger.setLevel(logging.INFO)
 _DEFAULT_SAFETENSOR_FILE_NAME = "model.safetensors.index.json"
 _CONFIG_NAME = "config.json"
 
+
 def get_hf_tokenizer(model_id: str) -> AutoTokenizer:
-    """ Get the HF tokenizer for a given model id """
+    """Get the HF tokenizer for a given model id"""
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     assert tokenizer is not None, f"Tokenizer not found for model id {model_id}"
     return tokenizer
+
 
 def init_buffers(
     stage_module: torch.nn.Module,
     device: torch.device,
     init_callbacks: Dict[str, Callable],
     model_config: Optional[Dict[str, str]] = None,
-    buffer_dtype: Optional[torch.dtype] = None, # torch.bfloat16,
+    buffer_dtype: Optional[torch.dtype] = None,  # torch.bfloat16,
 ) -> None:
-        """
-        Initialize buffers of `stage_module` using the callbacks in `init_callbacks`.
+    """
+    Initialize buffers of `stage_module` using the callbacks in `init_callbacks`.
 
-        Args:
-            stage_module (torch.nn.Module): The module whose buffers are to be initialized.
-            device (torch.device): The device on which to initialize the buffers.
-            init_callbacks (Dict[str, Callable]): A dictionary mapping buffer FQNs to their init functions.
-            model_config (Optional[Dict[str, str]]): Additional model configuration (unused in this function).
+    Args:
+        stage_module (torch.nn.Module): The module whose buffers are to be initialized.
+        device (torch.device): The device on which to initialize the buffers.
+        init_callbacks (Dict[str, Callable]): A dictionary mapping buffer FQNs to their init functions.
+        model_config (Optional[Dict[str, str]]): Additional model configuration (unused in this function).
 
-        Returns:
-            None
-        """
-        for name, buf in stage_module.named_buffers():
-            #logger.info(f"INSIDE - Checking buffer {name}, {buf=}")
-            for buffer_name_to_init, init_func in init_callbacks.items():
-                #logger.info(f"INSIDE - Checking buffer {name} against {buffer_name_to_init}")
-                if _should_initialize_buffer(name, buffer_name_to_init):
-                    #logger.info(f"INSIDE - About to Initializing buffer {name} with {init_func}")
-                    _initialize_buffer(stage_module, name, init_func, device, buffer_dtype )
-                    break
+    Returns:
+        None
+    """
+    for name, buf in stage_module.named_buffers():
+        # logger.info(f"INSIDE - Checking buffer {name}, {buf=}")
+        for buffer_name_to_init, init_func in init_callbacks.items():
+            # logger.info(f"INSIDE - Checking buffer {name} against {buffer_name_to_init}")
+            if _should_initialize_buffer(name, buffer_name_to_init):
+                # logger.info(f"INSIDE - About to Initializing buffer {name} with {init_func}")
+                _initialize_buffer(stage_module, name, init_func, device, buffer_dtype)
+                break
+
 
 def _should_initialize_buffer(buffer_name: str, pattern: str) -> bool:
-        """
-        Determine if a buffer should be initialized based on its name and a pattern.
+    """
+    Determine if a buffer should be initialized based on its name and a pattern.
 
-        Args:
-            buffer_name (str): The name of the buffer.
-            pattern (str): The pattern to match against.
+    Args:
+        buffer_name (str): The name of the buffer.
+        pattern (str): The pattern to match against.
 
-        Returns:
-            bool: True if the buffer should be initialized, False otherwise.
-        """
-        has_pattern, exact_buf_name = remove_pattern_prefix(pattern)
-        if has_pattern:
-            return exact_buf_name in buffer_name
-        else:
-             return exact_buf_name == buffer_name
-        return False
+    Returns:
+        bool: True if the buffer should be initialized, False otherwise.
+    """
+    has_pattern, exact_buf_name = remove_pattern_prefix(pattern)
+    if has_pattern:
+        return exact_buf_name in buffer_name
+    else:
+        return exact_buf_name == buffer_name
+    return False
 
-def _initialize_buffer(module: torch.nn.Module, buffer_name: str, init_func: Callable, device: torch.device, buffer_dtype=None) -> None:
+
+def _initialize_buffer(
+    module: torch.nn.Module,
+    buffer_name: str,
+    init_func: Callable,
+    device: torch.device,
+    buffer_dtype=None,
+) -> None:
     """
     Initialize a specific buffer in the module.
 
@@ -89,14 +100,14 @@ def _initialize_buffer(module: torch.nn.Module, buffer_name: str, init_func: Cal
     if buffer_dtype is not None:
         buf_val = buf_val.to(buffer_dtype)
         logger.info(f"Buffer dtype set to: {buf_val.dtype}")
-    module_path = buffer_name.split('.')
+    module_path = buffer_name.split(".")
     target_module = module
     for submodule in module_path[:-1]:
         target_module = getattr(target_module, submodule)
     target_module.register_buffer(module_path[-1], buf_val, persistent=False)
     logger.info(f"Initialized buffer {buffer_name}")
     logger.info(f"Buffer result: {target_module=}")
-    
+
 
 def open_hf_safetensor(file_path: str):
     try:
@@ -110,19 +121,19 @@ def open_hf_safetensor(file_path: str):
             tensors = {k: f.get_tensor(k) for k in f.keys()}
 
         return tensors
-    
+
     except Exception as e:
         print(f"An error occurred while opening the safetensor file: {str(e)}")
         return None
 
 
 def remove_pattern_prefix(s):
-    """ Remove the prefix 'pattern.' from a string and return bool re: if it was present 
+    """Remove the prefix 'pattern.' from a string and return bool re: if it was present
     example: has_pattern, result = remove_pattern_prefix(input_string)
     """
     prefix = "pattern."
     if s.startswith(prefix):
-        return True, s[len(prefix):]
+        return True, s[len(prefix) :]
     else:
         return False, s
 
@@ -135,20 +146,21 @@ def load_safetensor_weights(
     purge_model_prefix: bool = True,
     ignore_cache_layers: bool = True,
 ) -> Tuple[int, int]:
-    """ Load safetensor weights into a stage module.  
-    Returns the number of weights loaded and the number of missing weights. 
+    """Load safetensor weights into a stage module.
+    Returns the number of weights loaded and the number of missing weights.
     weight_map = {model_param: file_with_param}  # output.weight: model.safetensors.index.json
     new_to_old_keymap = {model_param: old_param}  # output.weight: lm_head.weight
     """
+
     def remove_model_prefix(d: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        return {k.removeprefix('model.'): v for k, v in d.items()}
+        return {k.removeprefix("model."): v for k, v in d.items()}
 
     stage_state_dict = stage_module.state_dict()
     if purge_model_prefix:
         stage_state_dict = remove_model_prefix(stage_state_dict)
         weight_map = remove_model_prefix(weight_map)
 
-    #logger.info(f"Stage state dict: len = {len(stage_state_dict)}, keys = {list(stage_state_dict.keys())}")
+    # logger.info(f"Stage state dict: len = {len(stage_state_dict)}, keys = {list(stage_state_dict.keys())}")
 
     updated_states = set()
     needed_files = {file for file in weight_map.values() if file is not None}
@@ -163,22 +175,28 @@ def load_safetensor_weights(
                 for param, file_with_param in weight_map.items():
                     if file_with_param == file and param in stage_state_dict:
                         # have to special case output.weight as only one not preceeded with model.
-                        if param =='output.weight':
-                            logger.info(f"skipping model prefix for {param} from {file_with_param}")
+                        if param == "output.weight":
+                            logger.info(
+                                f"skipping model prefix for {param} from {file_with_param}"
+                            )
                         else:
-                            model_param = 'model.' + param 
+                            model_param = "model." + param
 
                         old_param = new_to_old_keymap.get(model_param)
-                        
+
                         if old_param in checkpoint.keys():
                             checkpoint_tensor = checkpoint.get_tensor(old_param)
                             stage_tensor = stage_state_dict[param]
-                            checkpoint_tensor = compare_and_reverse(checkpoint_tensor, stage_tensor)
+                            checkpoint_tensor = compare_and_reverse(
+                                checkpoint_tensor, stage_tensor
+                            )
                             stage_state_dict[param] = checkpoint_tensor
                             updated_states.add(param)
                         else:
-                            logger.warning(f"**** Parameter {old_param} not found in checkpoint from {model_param}, skipping")
-    
+                            logger.warning(
+                                f"**** Parameter {old_param} not found in checkpoint from {model_param}, skipping"
+                            )
+
         except FileNotFoundError:
             logger.error(f"File not found: {full_path}")
         except Exception as e:
@@ -195,7 +213,9 @@ def load_safetensor_weights(
             logger.info(f"Ignoring {start_len - after_len} missing cache layers")
 
     if missing_keys:
-        logger.warning(f"Partially updated state dict. Missing {len(missing_keys)} keys: {missing_keys}")
+        logger.warning(
+            f"Partially updated state dict. Missing {len(missing_keys)} keys: {missing_keys}"
+        )
     else:
         logger.info("Fully updated state dict.")
 
@@ -203,7 +223,7 @@ def load_safetensor_weights(
     logger.info(f"Loaded {len(updated_states)} weights into stage module")
 
     return len(updated_states), len(missing_keys)
-  
+
 
 def read_weights_from_json(file_path: str) -> Optional[Dict[str, str]]:
     try:
@@ -219,8 +239,9 @@ def read_weights_from_json(file_path: str) -> Optional[Dict[str, str]]:
         print(f"An error occurred while reading the JSON file: {str(e)}")
         return None
 
-def get_config_file(model_id: str) -> Tuple[str,str]:
-    """ Get the config file and file location for a given HF model id """
+
+def get_config_file(model_id: str) -> Tuple[str, str]:
+    """Get the config file and file location for a given HF model id"""
     config_file = cached_file(model_id, _CONFIG_NAME)
     assert os.path.exists(config_file), f"Config file {config_file} does not exist."
     with open(config_file, "r") as file:
@@ -228,65 +249,72 @@ def get_config_file(model_id: str) -> Tuple[str,str]:
     file_location = os.path.dirname(config_file)
     return config_data, file_location
 
-def get_hf_weight_map_and_path(model_id: str) -> Tuple[Dict[str, str], str,]:
-    """ Get the weight map for a given HF model id and also the cache path for loading the weights """
+
+def get_hf_weight_map_and_path(
+    model_id: str,
+) -> Tuple[
+    Dict[str, str],
+    str,
+]:
+    """Get the weight map for a given HF model id and also the cache path for loading the weights"""
     index_file = cached_file(model_id, _DEFAULT_SAFETENSOR_FILE_NAME)
     print(f"Index file: {index_file}")
-    assert os.path.exists(index_file), f"Weight index file for {model_id} does not exist in HF cache...."
+    assert os.path.exists(
+        index_file
+    ), f"Weight index file for {model_id} does not exist in HF cache...."
     weight_map = read_weights_from_json(index_file)
 
     assert weight_map is not None, f"Weight map not found in config file {index_file}"
     weight_map, new_to_old_keymap = remap_weight_keys(weight_map)
-    
+
     weight_path = os.path.dirname(index_file)
     assert os.path.exists(weight_path), f"Weight path {weight_path} does not exist"
 
     return weight_map, weight_path, new_to_old_keymap
 
+
 def remap_weight_keys(dictionary):
-    """ Remap the keys of a dictionary to match the expected format of the tune model. """
+    """Remap the keys of a dictionary to match the expected format of the tune model."""
     replacements = {
-        'embed_tokens': 'tok_embeddings',
-        'input_layernorm.weight': 'sa_norm.scale',
-        'self_attn':'attn',
-        'o_proj':'output_proj',
-        'post_attention_layernorm.weight':'mlp_norm.scale',
-        'down_proj':'w1',
-        'gate_proj':'w2',
-        'up_proj':'w3',
-        'norm.weight':'norm.scale',
-        'lm_head.weight':'output.weight',
-        
+        "embed_tokens": "tok_embeddings",
+        "input_layernorm.weight": "sa_norm.scale",
+        "self_attn": "attn",
+        "o_proj": "output_proj",
+        "post_attention_layernorm.weight": "mlp_norm.scale",
+        "down_proj": "w1",
+        "gate_proj": "w2",
+        "up_proj": "w3",
+        "norm.weight": "norm.scale",
+        "lm_head.weight": "output.weight",
     }
-    
+
     new_dict = {}
     key_mapping = {}
-    
+
     for old_key, value in dictionary.items():
-        
         new_key = old_key
         for old_word, new_word in replacements.items():
             if old_word in new_key:
                 new_key = new_key.replace(old_word, new_word)
                 logger.info(f"Old key: {old_key}, {value=}, New key: {new_key}")
-        
+
         new_dict[new_key] = value
         key_mapping[new_key] = old_key
     return new_dict, key_mapping
 
+
 def compare_and_reverse(tensor1: torch.Tensor, tensor2: torch.Tensor) -> torch.Tensor:
-    """ Compare the shapes of two tensors and permute second one to match the first one. 
+    """Compare the shapes of two tensors and permute second one to match the first one.
     This is expressly used for mapping safetensor weights to the tune models.
     """
     # Compare the shapes of the two tensors
     shape1 = tensor1.shape
     shape2 = tensor2.shape
-    
+
     if len(shape1) == len(shape2):
         return tensor2
-    
+
     if shape1 == shape2[::-1]:
         return tensor2.permute(*range(tensor2.dim() - 1, -1, -1))
-    
+
     return tensor2
-    
--- distributed/pp_meta.py
+++ distributed/pp_meta.py
@@ -1,8 +1,7 @@
 # $ torchrun --nproc-per-node 4 pp_meta.py
 
 import os
-import json
-from typing import Optional, Dict, Tuple, Any, List
+from typing import Tuple, Any
 import logging
 from argparse import ArgumentParser
 
@@ -12,14 +11,20 @@ from torch.distributed.pipelining import pipeline, SplitPoint, ScheduleGPipe
 from torch._subclasses.fake_tensor import FakeTensorMode
 
 from utils import Color
-from modeling_utils import init_on_meta_device, check_rope_embedding, print_model_structure
+from modeling_utils import init_on_meta_device
 
-from torchtune.models.llama3 import llama3_8b, llama3_70b, Llama3Tokenizer
+from torchtune.models.llama3 import llama3_8b, llama3_70b
 from torchtune.models.llama3_1 import llama3_1_405b
-from hf_utils import get_hf_tokenizer, load_safetensor_weights, read_weights_from_json, get_hf_weight_map_and_path
-import time
+from hf_utils import (
+    get_hf_tokenizer,
+    load_safetensor_weights,
+    get_hf_weight_map_and_path,
+)
+
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -37,56 +42,69 @@ MODEL_CONFIGS = {
 }
 
 TUNE_MODEL_CONFIGS = {
-    "8b": (llama3_8b, 'meta-llama/Meta-Llama-3-8B-Instruct'),  # '/tmp/Meta-Llama-3-8B-Instruct/original/tokenizer.model'),
-    "70b": (llama3_70b, '/tmp/Meta-Llama-3-70B-Instruct/original/tokenizer.model'),
-    "405b": (llama3_1_405b, '/tmp/Meta-Llama-3.1-405B-Instruct/original/mp16/tokenizer.model'),
+    "8b": (
+        llama3_8b,
+        "meta-llama/Meta-Llama-3-8B-Instruct",
+    ),  # '/tmp/Meta-Llama-3-8B-Instruct/original/tokenizer.model'),
+    "70b": (llama3_70b, "/tmp/Meta-Llama-3-70B-Instruct/original/tokenizer.model"),
+    "405b": (
+        llama3_1_405b,
+        "/tmp/Meta-Llama-3.1-405B-Instruct/original/mp16/tokenizer.model",
+    ),
 }
 
-def create_model(model_id: str, device: str = "cuda", rank: int = 0) -> Tuple[Any, FakeTensorMode, Any]:
+
+def create_model(
+    model_id: str, device: str = "cuda", rank: int = 0
+) -> Tuple[Any, FakeTensorMode, Any]:
     fake_mode = FakeTensorMode(allow_non_fake_inputs=True)
-    
+
     model_func, hf_path = TUNE_MODEL_CONFIGS[model_id]
     print(f"{model_func=}, {hf_path=}")
 
     assert model_func is not None, f"Model {model_id} not found in TUNE_MODEL_CONFIGS"
-    assert hf_path is not None, f"hf path for {model_id} not found in TUNE_MODEL_CONFIGS"
+    assert (
+        hf_path is not None
+    ), f"hf path for {model_id} not found in TUNE_MODEL_CONFIGS"
 
     with init_on_meta_device(device="meta"):
         print(f"about to init model {model_id}")
         model = model_func().to(torch.bfloat16)
     model.eval()
-    if rank==0:
-        #print(model.config)
+    if rank == 0:
+        # print(model.config)
         print(f"{model=}")
-        #print(f"{model.model.embed_tokens.dtype=}")
+        # print(f"{model.model.embed_tokens.dtype=}")
         # print(f"{model.layers[0].attn.pos_embeddings.weight.dtype=}")
         print(f"{model.layers[0].attn.pos_embeddings=}")
-        print(f"{model.layers[0].attn.q_proj=}") 
+        print(f"{model.layers[0].attn.q_proj=}")
         print(f"{model.layers[0].attn.q_proj.weight.dtype=}")
-        
-    
+
     logger.info(f"Model type: {type(model)}")
-    #logger.info(f"Buffer callback: {model.buf_init_callbacks}")
-    #if not model.buf_init_callbacks:
+    # logger.info(f"Buffer callback: {model.buf_init_callbacks}")
+    # if not model.buf_init_callbacks:
     #    logger.warning("ROPE generation may not succeed - buf_init_callbacks is None")
-    
+
     with fake_mode:
-        model.to_empty(device='cuda')
-    
+        model.to_empty(device="cuda")
+
     # create tokenizer
-    #tokenizer = Llama3Tokenizer(tokenizer_path)
+    # tokenizer = Llama3Tokenizer(tokenizer_path)
     tokenizer = get_hf_tokenizer(hf_path)
     tokenizer.pad_token = tokenizer.eos_token
-    
+
     return model, fake_mode, tokenizer, hf_path
+
 
 def create_pipeline(model, inputs, world_size: int):
     # Split model into world_size stages
-    
+
     layers_per_rank = len(model.layers) // world_size
-    
-    logger.info(f"Splitting model into {world_size} stages with {layers_per_rank} layers each")
-    
+
+    logger.info(
+        f"Splitting model into {world_size} stages with {layers_per_rank} layers each"
+    )
+
     split_spec = {
         f"model.layers.{i * layers_per_rank}": SplitPoint.BEGINNING
         for i in range(1, world_size)
@@ -95,21 +113,25 @@ def create_pipeline(model, inputs, world_size: int):
     return pipeline(
         model,
         mb_args=(inputs,),
-        mb_kwargs=None, # {
+        mb_kwargs=None,  # {
         #    "output_attentions": False,
         #    "output_hidden_states": False,
         #    "use_cache": False,
-        #},
+        # },
         split_spec=split_spec,
     )
 
+
 # --- update init ----
+
 
 def main(model_id: str, world_size: int, device: str):
     rank = int(os.environ["RANK"])
     world_size_dist = int(os.environ["WORLD_SIZE"])
     if world_size_dist != world_size:
-        logger.warning(f"World size mismatch: {world_size_dist} != {world_size}. Overriding with dist world size")
+        logger.warning(
+            f"World size mismatch: {world_size_dist} != {world_size}. Overriding with dist world size"
+        )
         world_size = world_size_dist
     logger.info(f"Rank: {rank} / {world_size}")
 
@@ -118,7 +140,7 @@ def main(model_id: str, world_size: int, device: str):
 
     # Create model on meta device
     model, fake_mode, tokenizer, hf_path = create_model(model_id, device, rank)
-    
+
     print(f"{tokenizer.pad_token=}")
 
     prompts = ("How do you", "I like to")
@@ -138,18 +160,24 @@ def main(model_id: str, world_size: int, device: str):
     logger.info("Materializing each stage...")
     stage_module = pipe.get_stage_module(rank)
     logger.info(f"Stage module type: {type(stage_module)}")
-    
+
     logger.info(f"Loading weights into stage {rank}")
-    total_weight_count, missing_weight_count = load_safetensor_weights(stage_module, weight_map, weight_path, new_to_old_keymap)
-    
-    logger.info(f"Loaded {total_weight_count} weights into stage {rank} with {missing_weight_count} missing")
-    assert missing_weight_count == 0, f"Missing {missing_weight_count} weights in stage {rank}"
-    
+    total_weight_count, missing_weight_count = load_safetensor_weights(
+        stage_module, weight_map, weight_path, new_to_old_keymap
+    )
+
+    logger.info(
+        f"Loaded {total_weight_count} weights into stage {rank} with {missing_weight_count} missing"
+    )
+    assert (
+        missing_weight_count == 0
+    ), f"Missing {missing_weight_count} weights in stage {rank}"
+
     if rank == 0:
         logger.info(f"After load safe tensor Stage module type: {type(stage_module)}")
 
-    dist.barrier() # wait for all ranks to finish loading weights
-    '''logger.info("About to try to init buffers")
+    dist.barrier()  # wait for all ranks to finish loading weights
+    """logger.info("About to try to init buffers")
     if hasattr(model, "buf_init_callbacks"):
         logger.info(f"Initializing buffers with device={device}")
         init_buffers(stage_module, device, model.buf_init_callbacks, model_config)
@@ -158,34 +186,41 @@ def main(model_id: str, world_size: int, device: str):
         #logger.info(f"{Color.blue}{type(stage_module)=} {dir(stage_module)=}{Color.reset}")
         logger.info(f"{Color.blue}{stage_module.model.rotary_emb=}{Color.reset}")
         logger.info(f"{Color.blue}{stage_module.model.rotary_emb.inv_freq.dtype=}{Color.reset}")
-    '''
-    #else:
+    """
+    # else:
     #    logger.info(f"{Color.blue}{type(stage_module)=} {dir(stage_module)=}{Color.reset}")
-    
-    logger.info(f"{Color.green}\n--->  {Color.yellow}{rank=} {Color.blue}Successfully traced, segmented and loaded weights for model {Color.green}{model_id}{Color.reset}")
+
+    logger.info(
+        f"{Color.green}\n--->  {Color.yellow}{rank=} {Color.blue}Successfully traced, segmented and loaded weights for model {Color.green}{model_id}{Color.reset}"
+    )
     dist.barrier()
 
     # Create schedule runtime
     stage = pipe.build_stage(rank, device=device)
-    #if rank == 0:
+    # if rank == 0:
     #    logger.info(f"{rank=} Completed stage building:  {stage=}...")
-        #logger.info(f"{Color.blue}{type(stage_module)=} {dir(stage_module)=}{Color.reset}")
+    # logger.info(f"{Color.blue}{type(stage_module)=} {dir(stage_module)=}{Color.reset}")
 
     logger.info("Pipeline Complete ---- Running schedule...")
 
     logger.info(f"{rank=} Completed stage building:  {stage=}...")
-    #logger.info(f"{Color.blue}{type(stage_module)=} {dir(stage_module)=}{Color.reset}")
-    #logger.info(f"{Color.blue}{stage_module.print_readable()=}{Color.reset}")
+    # logger.info(f"{Color.blue}{type(stage_module)=} {dir(stage_module)=}{Color.reset}")
+    # logger.info(f"{Color.blue}{stage_module.print_readable()=}{Color.reset}")
 
     # Run
     # Run time inputs
     full_batch_prompts = (
-        "How do you", "I like to", "Can I help", "You need to",
-        "The weather is", "I found a", "What is your", "You are so",
+        "How do you",
+        "I like to",
+        "Can I help",
+        "You need to",
+        "The weather is",
+        "I found a",
+        "What is your",
+        "You are so",
     )  # full batch size = 8
-    
-    inputs = tokenizer(full_batch_prompts, return_tensors="pt", padding=True).to(device)
 
+    inputs = tokenizer(full_batch_prompts, return_tensors="pt", padding=True).to(device)
 
     # Attach to a schedule
     # number of microbatches = 8 // 2 = 4
@@ -193,10 +228,9 @@ def main(model_id: str, world_size: int, device: str):
     schedule = ScheduleGPipe(stage, num_mbs)
 
     if rank == 0:
-        output = schedule.step(inputs['input_ids'])
+        output = schedule.step(inputs["input_ids"])
     else:
         output = schedule.step()
-    
 
     # Decode
     if output is not None:
@@ -204,14 +238,14 @@ def main(model_id: str, world_size: int, device: str):
         logger.info(f"Output from schedule step {output=}")
         next_token_logits = output[:, -1, :]
         next_token = torch.argmax(next_token_logits, dim=-1)
-        logger.info(f"First Pass Generation------")
+        logger.info("First Pass Generation------")
         logger.info(f"{next_token=}")
         logger.info(f"Results = {tokenizer.batch_decode(next_token)}")
-    #else:
+    # else:
     #    logger.info(f"Output from schedule step is None {output=}")
     dist.barrier()
     dist.destroy_process_group()
-    
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Model tracing and segmentation")
