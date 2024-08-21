09c7bfe26e6b8f270554295a35248c3dcb7cff89
Ke Wen <kw2501@meta.com>
2024-08-21 10:48:35 -0700

    Initial add of distributed model

---
+++ build/dist_run.py
@@ -0,0 +1,33 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.distributed as dist
+
+from model_dist import TransformerArgs, Transformer
+
+# Model config
+config = TransformerArgs.from_name("Transformer-2-7b-chat-hf")
+print(config)
+
+# Construct a device mesh with available devices (multi-host or single host)
+device_mesh = dist.init_device_mesh("cuda", (2,), mesh_dim_names=("tp",))
+rank = dist.get_rank()
+device = torch.device(f"cuda:{rank}")
+
+# Create parallel model with device_mesh context
+with device:
+    with device_mesh:
+        model = Transformer(config)
+        model.setup_caches(1, 4096)
+
+print(model)
+
+# Distributed run
+input_ids = torch.randint(0, config.vocab_size, (1, 4096), device=device)
+input_pos = torch.arange(0, 4096, device=device)
+output = model(input_ids, input_pos)
+print(f"Rank {rank} completes.")
