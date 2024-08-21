a3f742407069ac86ca70f601696bcffb24b45fc2
Ke Wen <kw2501@meta.com>
2024-08-21 11:23:58 -0700

    Remove unused imports

--- build/model_dist.py
+++ build/model_dist.py
@@ -3,10 +3,6 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-import json
-import os
-
-from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, Optional
 
