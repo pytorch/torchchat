5d0f871bbeb49ef3acb07371d8a3eba1e93d782e
Jack-Khuu <jackkhuu@fb.com>
2024-08-28 10:55:25 -0700

    Fix :W typo + debug print

--- .ci/scripts/validate.sh
+++ .ci/scripts/validate.sh
@@ -286,7 +286,7 @@ function eval_model_sanity_check() {
             echo "*************************************************"
             if [ "$DTYPE" != "float16" ]; then
                 python3 -W ignore torchchat.py export --dtype ${DTYPE} --quant "$QUANT_OPTIONS" --checkpoint-path "$CHECKPOINT_PATH" --output-dso-path ${MODEL_DIR}/${MODEL_NAME}.so --dynamic-shapes --device "$TARGET_DEVICE" || exit 1
-                python3 -W ignore torchchat.py eval --dtype ${DTYPE} --checkpoint-path "$CHECKPOINT_PATH" --dso-path ${MODEL_DIR}/${MOD:WEL_NAME}.so --device "$TARGET_DEVICE" --limit 5 > "$MODEL_DIR/output_eval_aoti" || exit 1
+                python3 -W ignore torchchat.py eval --dtype ${DTYPE} --checkpoint-path "$CHECKPOINT_PATH" --dso-path ${MODEL_DIR}/${MODEL_NAME}.so --device "$TARGET_DEVICE" --limit 5 > "$MODEL_DIR/output_eval_aoti" || exit 1
                 cat "$MODEL_DIR/output_eval_aoti"
             fi;
         fi;
