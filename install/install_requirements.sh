e32005f153479e92f290e525d37015944f7e810d
gasoonjia <gasoonjia@icloud.com>
2024-09-11 11:48:18 -0700

    bring torchao back to all platform

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -95,11 +95,19 @@ LINUX_REQUIREMENTS_TO_INSTALL=(
 )
 
 PLATFORM=$(uname -s)
-if [ "$PLATFORM" == "Linux" ]; then
+if [ "$PLATFORM" == "Linux" ];
+then
   (
     set -x
     $PIP_EXECUTABLE install --pre --extra-index-url "${TORCH_NIGHTLY_URL}" --no-cache-dir \
       "${LINUX_REQUIREMENTS_TO_INSTALL[@]}"
+  )
+else
+  # For torchao need to install from github since nightly build doesn't have macos build.
+  # TODO: Remove this and install nightly build, once it supports macos
+  (
+    set -x
+    $PIP_EXECUTABLE install git+https://github.com/pytorch/ao.git@e11201a62669f582d81cdb33e031a07fb8dfc4f3
   )
 fi
 
