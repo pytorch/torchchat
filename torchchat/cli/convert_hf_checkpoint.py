e6ed31a714265be151134f1a65896a578426c254
Jack-Khuu <jackkhuu@fb.com>
2024-09-18 17:07:26 -0700

    Refix by casting to TransformerArgs

--- torchchat/cli/convert_hf_checkpoint.py
+++ torchchat/cli/convert_hf_checkpoint.py
@@ -12,6 +12,8 @@ from typing import Optional
 
 import torch
 
+from torchchat.model import TransformerArgs
+
 # support running without installing as a package
 wd = Path(__file__).parent.parent
 sys.path.append(str(wd.resolve()))
@@ -32,8 +34,9 @@ def convert_hf_checkpoint(
     if model_name is None:
         model_name = model_dir.name
 
-    config = ModelArgs.from_name(model_name).transformer_args['text']
-    print(f"Model config {config}")
+    config_args = ModelArgs.from_name(model_name).transformer_args['text']
+    config = TransformerArgs.from_params(config_args)
+    print(f"Model config {config.__dict__}")
 
     # Load the json file containing weight mapping
     model_map_json = model_dir / "pytorch_model.bin.index.json"
