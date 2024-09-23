db88dc84e5d9a3d2f7baf8d316dcbcd9f06f4d9f
Mengwei Liu <larryliu@meta.com>
2024-09-23 10:11:00 -0700

    Add import for quantized decomposed ops
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torchchat/model.py
+++ torchchat/model.py
@@ -932,6 +932,9 @@ try:
     from executorch.extension.pybindings import portable_lib as exec_lib
 
     # ET changed the way it's loading the custom ops so it's not included in portable_lib but has to be loaded separately.
+    # For quantized_decomposed ops
+    from executorch.kernels import quantized  # no-qa
+    # For llama::sdpa_with_kv_cache.out, preprocess ops
     from executorch.extension.llm.custom_ops import sdpa_with_kv_cache  # no-qa
 
     class PTEModel(nn.Module):
