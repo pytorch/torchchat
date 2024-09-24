3f3bd8db4b64e60941f9037463264544e5fcb2ce
Scott Wolchok <swolchok@meta.com>
2024-09-24 09:48:17 -0700

    parallelize ExecuTorch build
    ghstack-source-id: 333693ea1ba047589189babadf803f4dc54f7c65
    Pull Request resolved: https://github.com/pytorch/torchchat/pull/1168

--- torchchat/utils/scripts/install_utils.sh
+++ torchchat/utils/scripts/install_utils.sh
@@ -150,7 +150,7 @@ install_executorch_cpp_libs() {
         -DEXECUTORCH_BUILD_KERNELS_CUSTOM=${EXECUTORCH_BUILD_KERNELS_CUSTOM_VAR} \
         ${CROSS_COMPILE_ARGS} \
         -S . -B ${CMAKE_OUT_DIR} -G Ninja
-  cmake --build ${CMAKE_OUT_DIR}
+  cmake --build ${CMAKE_OUT_DIR} -j16
   cmake --install ${CMAKE_OUT_DIR} --prefix ${TORCHCHAT_ROOT}/${ET_BUILD_DIR}/install
   popd
 }
