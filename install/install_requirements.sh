50ff6026261547484a5fb1b7e97f3f8343929b08
Jack-Khuu <jackkhuu@fb.com>
2024-09-25 14:07:08 -0700

    Move backto nightly for torchtune

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -52,6 +52,10 @@ PYTORCH_NIGHTLY_VERSION=dev20240901
 # Nightly version for torchvision
 VISION_NIGHTLY_VERSION=dev20240901
 
+# Nightly version for torchtune
+TUNE_NIGHTLY_VERSION=dev20240925
+
+
 # Uninstall triton, as nightly will depend on pytorch-triton, which is one and the same
 (
   set -x
@@ -72,6 +76,7 @@ fi
 REQUIREMENTS_TO_INSTALL=(
   torch=="2.5.0.${PYTORCH_NIGHTLY_VERSION}"
   torchvision=="0.20.0.${VISION_NIGHTLY_VERSION}"
+  torchtune=="0.3.0.${TUNE_NIGHTLY_VERSION}"
 )
 
 # Install the requirements. --extra-index-url tells pip to look for package
