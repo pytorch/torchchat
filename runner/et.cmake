1980a69d6ded01cd0fa2df770b80bc34b09ade5a
Scott Roy <161522778+metascroy@users.noreply.github.com>
2024-09-27 13:02:50 -0700

    Add torchao (#1182)
    * init
    
    * update install utils
    
    * update
    
    * update libs
    
    * update torchao pin
    
    * fix ci test
    
    * add python et install to ci
    
    * fix ci errors
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes

--- runner/et.cmake
+++ runner/et.cmake
@@ -116,6 +116,14 @@ if(executorch_FOUND)
     target_link_libraries(et_run PRIVATE log)
   endif()
 
+  if(LINK_TORCHAO_OPS)
+    target_link_libraries(et_run PRIVATE "$<LINK_LIBRARY:WHOLE_ARCHIVE,${TORCHCHAT_ROOT}/torchao-build/cmake-out/lib/liblinear_a8wxdq_EXECUTORCH.a>")
+    target_link_libraries(et_run PRIVATE
+      "${TORCHCHAT_ROOT}/torchao-build/cmake-out/lib/libtorchao_kernels_aarch64.a"
+       "${TORCHCHAT_ROOT}/torchao-build/cmake-out/lib/libtorchao_ops_linear_EXECUTORCH.a"
+    )
+  endif()
+
 else()
   MESSAGE(WARNING "ExecuTorch package not found")
 endif()
--- torchchat/utils/scripts/build_torchao_ops.sh
+++ torchchat/utils/scripts/build_torchao_ops.sh
@@ -5,18 +5,12 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-set -ex pipefail
+
 
 source "$(dirname "${BASH_SOURCE[0]}")/install_utils.sh"
 
-if [ "${ET_BUILD_DIR}" == "" ]; then
-  ET_BUILD_DIR="et-build"
-fi
-
-ENABLE_ET_PYBIND="${1:-true}"
-
 pushd ${TORCHCHAT_ROOT}
 find_cmake_prefix_path
-clone_executorch
-install_executorch_libs $ENABLE_ET_PYBIND
+clone_torchao
+install_torchao_aten_ops
 popd
