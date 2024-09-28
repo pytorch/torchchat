ce4f2bf4d2c8ff6317a0a739e1604116c14c7413
Jack-Khuu <jackkhuu@fb.com>
2024-09-28 11:28:18 -0700

    Fix Evaluate to a version

--- torchchat/model.py
+++ torchchat/model.py
@@ -32,7 +32,6 @@ from torch.distributed.tensor.parallel import (
 from torch.nn import functional as F
 # TODO: remove this after we figure out where in torchtune an `evaluate` module
 # is being imported, which is being confused with huggingface's `evaluate``.
-import lm_eval  # noqa 
 from torchtune.models.clip import clip_vision_encoder
 from torchtune.models.llama3_1._component_builders import llama3_1 as llama3_1_builder
 from torchtune.models.llama3_2_vision._component_builders import (
