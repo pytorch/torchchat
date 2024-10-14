c867660049c6bbceefbaa2341f7d9c79832f2f7f
Hansong <107070759+kirklandsign@users.noreply.github.com>
2024-10-13 18:58:43 -0700

    Update torchchat Android app to use latest ET llama demo app (#1292)

---
+++ torchchat/edge/android/torchchat/app/src/main/java/org/pytorch/torchchat/ModelType.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package org.pytorch.torchchat;
+
+public enum ModelType {
+  LLAMA_3,
+  LLAMA_3_1,
+  LLAMA_3_2,
+  LLAVA_1_5,
+  LLAMA_GUARD_3,
+}
