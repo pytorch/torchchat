d25d3e610ee0bd3a5bd6c63817397bef8846aeb1
Gabe Goodhart <ghart@us.ibm.com>
2024-10-24 16:47:02 -0600

    test: Extract testing into a helper function
    https://github.com/pytorch/torchchat/issues/1251
    Branch: TokenizersCpp-1251
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- tokenizer/tests/pre_tokenizer_test.cpp
+++ tokenizer/tests/pre_tokenizer_test.cpp
@@ -14,22 +14,33 @@
 #include "pre_tokenizer.h"
 
 
-// RegexPreTokenizer ///////////////////////////////////////////////////////////
+// Helpers /////////////////////////////////////////////////////////////////////
 
-class RegexPreTokenizerTest : public ::testing::Test {};
 
-TEST_F(RegexPreTokenizerTest, Construct) {
-    RegexPreTokenizer tok("[0-9]+");
+void assert_split_match(
+  const PreTokenizer& tok,
+  const std::string& prompt,
+  const std::vector<std::string>& expected
+) {
+  re2::StringPiece prompt_view(prompt);
+  const auto& got = tok.pre_tokenize(prompt_view);
+  EXPECT_EQ(got.size(), 5);
+  for (auto i = 0; i < got.size(); ++i) {
+    EXPECT_EQ(got[i], expected[i]);
+  }
 }
 
-TEST_F(RegexPreTokenizerTest, PatternSplit) {
-    RegexPreTokenizer tok(R"((?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+)");
-    std::string text = "How are you doing?";
-    std::vector<std::string> expected = {"How", " are", " you", " doing", "?"};
-    re2::StringPiece text_view(text);
-    const auto res = tok.pre_tokenize(text_view);
-    EXPECT_EQ(res.size(), 5);
-    for (auto i = 0; i < res.size(); ++i) {
-        EXPECT_EQ(res[i], expected[i]);
-    }
+
+// RegexPreTokenizer ///////////////////////////////////////////////////////////
+class RegexPreTokenizerTest : public ::testing::Test {};
+
+// Test the basic construction
+TEST_F(RegexPreTokenizerTest, Construct) {
+  RegexPreTokenizer tok("[0-9]+");
+}
+
+// Test basic splitting using the expression for Tiktoken
+TEST_F(RegexPreTokenizerTest, TiktokenExpr) {
+  RegexPreTokenizer tok(R"((?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+)");
+  assert_split_match(tok, "How are you doing?", {"How", " are", " you", " doing", "?"});
 }
