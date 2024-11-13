803c41e233b749fff9380fbc22c72d88ed06f242
Jack-Khuu <jack.khuu.7@gmail.com>
2024-11-12 19:06:58 -0800

    Update install_requirements.sh

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -14,19 +14,21 @@ then
   if [[ -z ${CONDA_DEFAULT_ENV:-} ]] || [[ ${CONDA_DEFAULT_ENV:-} == "base" ]] || [[ ! -x "$(command -v python)" ]];
   then
     PYTHON_EXECUTABLE=python3
+  else
+    PYTHON_EXECUTABLE=python
   fi
 fi
 echo "Using python executable: $PYTHON_EXECUTABLE"
 
 PYTHON_SYS_VERSION="$($PYTHON_EXECUTABLE -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")"
-# Check python version. Expect 3.10.x to 3.12.x. Note 3.13 may work, but no guarantees
+# Check python version. Expect greater than 3.10.x
 if ! $PYTHON_EXECUTABLE -c "
 import sys
-if sys.version_info < (3, 10) or sys.version_info >= (3, 13):
+if sys.version_info < (3, 10):
     sys.exit(1)
 ";
 then
-  echo "Python version must be between 3.10.x and 3.12.x. Detected version: $PYTHON_SYS_VERSION"
+  echo "Python version must be greater than 3.10.x. Detected version: $PYTHON_SYS_VERSION"
   exit 1
 fi
 
