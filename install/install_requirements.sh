59e00d50795aab67bdd2a67c1b2e5896a26f42ae
Jack-Khuu <jack.khuu.7@gmail.com>
2024-12-06 15:42:17 -0800

    Test nightly with updated vision

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -62,10 +62,10 @@ echo "Using pip executable: $PIP_EXECUTABLE"
 # NOTE: If a newly-fetched version of the executorch repo changes the value of
 # PYTORCH_NIGHTLY_VERSION, you should re-run this script to install the necessary
 # package versions.
-PYTORCH_NIGHTLY_VERSION=dev20241126
+PYTORCH_NIGHTLY_VERSION=dev20241206
 
 # Nightly version for torchvision
-VISION_NIGHTLY_VERSION=dev20241126
+VISION_NIGHTLY_VERSION=dev20241206
 
 # Nightly version for torchtune
 TUNE_NIGHTLY_VERSION=dev20241126
